08b681b846503e8294e9a0a9ac18c098c1d434e9
Daniel Kirchner <daniel.c.kirchner@googlemail.com>
2015-05-24 15:20:47 +0200

    .

--- src/main/scala/de/haw/bachelorthesis/dkirchner/RealtimeAnalyzer.scala
+++ src/main/scala/de/haw/bachelorthesis/dkirchner/RealtimeAnalyzer.scala
@@ -77,7 +77,7 @@ object RealtimeAnalyzer {
         status.getText.split(" ") // TODO: bessere filter?
           .map(word =>
             scores.apply(hashingTF.indexOf(word.toLowerCase))).reduce(_ + _)
-          / status.getText.split(" ").length //TODO optimieren, schoener machen
+          ./(status.getText.split(" ").length)
         , status)
       )//.transform(_.sortByKey())
     }
